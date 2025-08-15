7998d501819910371aabc6d5ebf41f2dbfdfde8e
DST81 <148617124+DST81@users.noreply.github.com>
2025-08-15 10:52:42 +0200

    Update 2_Arbeitsplanung_alternativ.py

--- pages/2_Arbeitsplanung_alternativ.py
+++ pages/2_Arbeitsplanung_alternativ.py
@@ -86,7 +86,7 @@ st.title('Arbeitsplanung - Termine')
 if "start_date" not in st.session_state:
     #aktueller Wochenanfang(Montag)
     today=date.today()
-    st.session_state.start_date=today-timedelat(days=today.weekday())
+    st.session_state.start_date=today-timedelta(days=today.weekday())
 col1, col2, col3 = st.columns([1,2,1])
 with col1:
     if st.button("⬅ Vorwoche"):
