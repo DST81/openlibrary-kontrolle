6f824afbc16a6e5d4c781ec6e3a82878fdb0a2b8
DST81 <148617124+DST81@users.noreply.github.com>
2025-08-15 18:12:18 +0200

    Update 4_grüneFelder.py

--- pages/4_grüneFelder.py
+++ pages/4_grüneFelder.py
@@ -226,7 +226,7 @@ zeit_slot = st.selectbox('Zeit', zeiten)
 selected_personen = default_oeffnungszeiten.get(zeit_slot, [])
 oeffnungszeiten = st.multiselect("Wer übernimmt die Ausleihe?", list(avatars.keys()), default=selected_personen)
 klassenbesuch = st.text_input("Klassenbesuch (optional)", value=default_klassenbesuch)
-bemerkung = st.text_area("Bemerkung (optional), value=default_bemerkung")
+bemerkung = st.text_area("Bemerkung (optional)", value=default_bemerkung)
  
       
 if st.button("💾 Speichern"):
