adce4512638163354ead2a9ff493c8ada3bd8d26
DST81 <148617124+DST81@users.noreply.github.com>
2025-08-15 08:49:57 +0200

    Update 1_Arbeitsplanung.py

--- pages/1_Arbeitsplanung.py
+++ pages/1_Arbeitsplanung.py
@@ -84,7 +84,7 @@ events=[]
 for tag, details in planung.items():
   if 'oeffnungszeiten' in details and details['oeffnungszeiten']:
     events.append({
-      'title':'Öffnungszeiten: ' +', '.join(details['oeffnungszeiten']),
+      'title':'👤 ' +', '.join(details['oeffnungszeiten']),
       'start':tag
     })
     if "klassenbesuch" in details and details["klassenbesuch"]:
